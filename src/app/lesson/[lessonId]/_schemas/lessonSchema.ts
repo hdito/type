c6330d14dc46436a3ad1cf49424aea6aa596dca0
hdito <hditow@gmail.com>
2023-08-28 16:58:55 +0300

    Update lesson schema to limit lines length

--- src/app/lesson/[lessonId]/_schemas/lessonSchema.ts
+++ src/app/lesson/[lessonId]/_schemas/lessonSchema.ts
@@ -1,12 +1,39 @@
 import { z } from "zod";
 
+const splitLongLines = z
+  .string()
+  .trim()
+  .transform((str) => {
+    let transformedString = str;
+    while (!transformedString.split("\n").every((line) => line.length <= 50)) {
+      transformedString = transformedString
+        .split("\n")
+        .map((line) => {
+          if (line.length <= 50) {
+            return line;
+          }
+          const whitespaceIndex = line.lastIndexOf(" ", 50);
+          if (whitespaceIndex === -1) {
+            throw Error("Invalid lesson format");
+          }
+          return (
+            line.slice(0, whitespaceIndex) +
+            "\n" +
+            line.slice(whitespaceIndex + 1)
+          );
+        })
+        .join("\n");
+    }
+    return transformedString;
+  });
+
 const LessonPageSchema = z.union([
   z.object({
-    text: z.string().trim().optional(),
+    text: splitLongLines.optional(),
     description: z.string().trim(),
   }),
   z.object({
-    text: z.string().trim(),
+    text: splitLongLines,
     description: z.string().trim().optional(),
   }),
 ]);
